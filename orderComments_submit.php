26d2fecf31d7b70519247d5d8e0a3f3908078c23
jonaslb <jonasbakker1@gmail.com>
2017-08-04 14:57:10 +0200

    Update orderComments_submit.php

--- orderComments_submit.php
+++ orderComments_submit.php
@@ -7,19 +7,19 @@ include "saveSubjectToVariable.php";
 <?php
 
 if($_POST['commentorder'] == "date_newFirst"){
-	echo ("<SCRIPT LANGUAGE='JavaScript'>window.location.href='index.php?subject=$subject&sortBy=dateDESC';</SCRIPT>");
+	echo ("<SCRIPT LANGUAGE='JavaScript'>window.location.href='index.php?subject=$subject&sortBy=dateDESC#commentsection';</SCRIPT>");
 }
 
 if($_POST['commentorder'] == "date_newLast"){
-	echo ("<SCRIPT LANGUAGE='JavaScript'>window.location.href='index.php?subject=$subject&sortBy=dateASC';</SCRIPT>");
+	echo ("<SCRIPT LANGUAGE='JavaScript'>window.location.href='index.php?subject=$subject&sortBy=dateASC#commentsection';</SCRIPT>");
 }
 
 if($_POST['commentorder'] == "rating_bestFirst"){
-	echo ("<SCRIPT LANGUAGE='JavaScript'>window.location.href='index.php?subject=$subject&sortBy=ratingASC';</SCRIPT>");
+	echo ("<SCRIPT LANGUAGE='JavaScript'>window.location.href='index.php?subject=$subject&sortBy=ratingASC#commentsection';</SCRIPT>");
 }
 
 if($_POST['commentorder'] == "rating_worstFirst"){
-	echo ("<SCRIPT LANGUAGE='JavaScript'>window.location.href='index.php?subject=$subject&sortBy=ratingDESC';</SCRIPT>");
+	echo ("<SCRIPT LANGUAGE='JavaScript'>window.location.href='index.php?subject=$subject&sortBy=ratingDESC#commentsection';</SCRIPT>");
 }
 
 ?>
