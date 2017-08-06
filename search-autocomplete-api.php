ed1b1ead49db389749b69480bf10501c5994874b
ueehp <ueehp@student.kit.edu>
2017-08-06 10:02:17 +0200

    adds sql injection security to autocomplete search

--- search-autocomplete-api.php
+++ search-autocomplete-api.php
@@ -21,7 +21,12 @@ $module_result ="";
 $institute_result ="";
 
 if (isset($_GET['query'])){
-	$lectures = mysqli_query($con,"SELECT subject_name,code FROM subjects WHERE subject_name LIKE '%".$_GET['query']."%' ORDER BY subject_name LIMIT 5");
+	$query = '%'.$_GET['query'].'%';
+	
+	$statement = $con->prepare("SELECT subject_name,code FROM subjects WHERE subject_name LIKE ? ORDER BY subject_name LIMIT 5");
+	$statement->bind_param('s', $query);
+	$statement->execute();
+	$lectures = $statement->get_result();
 	while($row = mysqli_fetch_assoc($lectures)){
 		if($count < 3){
 			$lecture_result .= '{ "value" :"'.$row['subject_name'].'", "data": {"category" : "Veranstaltung", "dest":"index.php?subject='.$row['code'].'" } },';
@@ -34,7 +39,10 @@ if (isset($_GET['query'])){
 	$counttotal = $count;
 	$count = 0;
 	
-	$modules = mysqli_query($con,"SELECT name, module_ID FROM modules WHERE name LIKE '%".$_GET['query']."%' ORDER BY name LIMIT 5");
+	$statement = $con->prepare("SELECT name, module_ID FROM modules WHERE name LIKE ? ORDER BY name LIMIT 5");
+	$statement->bind_param('s', $query);
+	$statement->execute();
+	$modules = $statement->get_result();
 	while($row = mysqli_fetch_assoc($modules)){
 		if($count < 3){
 			$module_result .= '{ "value" :"'.$row['name'].'", "data": {"category" : "Module", "dest":"module.php?module_id='.$row['module_ID'].'"} },';
@@ -47,7 +55,10 @@ if (isset($_GET['query'])){
 	$counttotal += $count;
 	$count = 0;
 	
-	$institutes = mysqli_query($con,"SELECT name, abbr, institute_ID FROM institutes WHERE abbr LIKE '%".$_GET['query']."%' OR name LIKE '%".$_GET['query']."%' ORDER BY abbr LIMIT 5");
+	$statement = $con->prepare("SELECT name, abbr, institute_ID FROM institutes WHERE abbr LIKE ? OR name LIKE ? ORDER BY abbr LIMIT 5");
+	$statement->bind_param('s', $query, $query);
+	$statement->execute();
+	$institutes = $statement->get_result();
 	while(($row = mysqli_fetch_assoc($institutes)) && $count<2){
 		$institute_result .= '{ "value" :"('.$row['abbr'].') '.$row['name'].'", "data": {"category" : "Institute", "dest":"institute.php?institute_id='.$row['institute_ID'].'" } },';
 		$anything = true;
@@ -56,7 +67,10 @@ if (isset($_GET['query'])){
 	$counttotal += $count;
 	$count = 0;
 	
-	$lecturers = mysqli_query($con,"SELECT first_name, last_name, lecturer_ID FROM lecturers WHERE CONCAT(first_name,' ',last_name) LIKE '%".$_GET['query']."%'  ORDER BY last_name LIMIT 4");
+	$statement = $con->prepare("SELECT first_name, last_name, lecturer_ID FROM lecturers WHERE CONCAT(first_name,' ',last_name) LIKE ? ORDER BY last_name LIMIT 4");
+	$statement->bind_param('s', $query);
+	$statement->execute();
+	$lecturers = $statement->get_result();
 	while(($row = mysqli_fetch_assoc($lecturers) )&& $count<2 && $counttotal<8){
 		$lecturer_result .= '{ "value" :"'.$row['last_name'].', '.$row['first_name'].'", "data": {"category" : "Dozenten", "dest":"lecturer.php?lecturer_id='.$row['lecturer_ID'].'" } },';
 		$anything = true;
@@ -104,4 +118,5 @@ if (isset($_GET['query'])){
 }
 
 echo $result;
+$statement->close();
 ?>
