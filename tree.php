5cbde286e653f86b7fed4bf0934362dd080e9aea
jonas-l-b <ueehp@student.kit.edu>
2017-08-07 15:58:50 +0200

    führt feste tabellenzeilengröße ein

--- tree.php
+++ tree.php
@@ -366,15 +366,15 @@ include "connect.php";
 			foreach($data as $item){
 				$content .= "
 					<tr>
-						<td><a href=\"index.php?subject=".$item['subject_code']."\" target=\"blank\">".$item['subject_name']."</a></td>
-						<td>".$item['modul_types']."</td>
-						<td>".$item['part_of_modules']."</td>
-						<td>".$item['levels']."</td>
-						<td>".$item['ECTS']."</td>
-						<td>".$item['lecturers']."</td>
-						<td>".$item['semester']."</td>
-						<td>".$item['language']."</td>
-						<td>".$item[$orderBy]." %</td>
+						<td><div><a href=\"index.php?subject=".$item['subject_code']."\" target=\"blank\">".$item['subject_name']."</a></div></td>
+						<td><div>".$item['modul_types']."</div></td>
+						<td><div>".$item['part_of_modules']."</div></td>
+						<td><div>".$item['levels']."</div></td>
+						<td><div>".$item['ECTS']."</div></td>
+						<td><div>".$item['lecturers']."</div></td>
+						<td><div>".$item['semester']."</div></td>
+						<td><div>".$item['language']."</div></td>
+						<td><div>".$item[$orderBy]." %</div></td>
 					</tr>
 				";
 			}
@@ -382,7 +382,7 @@ include "connect.php";
 			$orderByHeader = "";
 			switch($orderBy){
 				case "overall":
-					$orderByHeader = "Gesamtbewertung";
+					$orderByHeader = "Gesamtwertung";
 					break;
 				case "crit1":
 					$orderByHeader = "Kriterium 1";
@@ -686,7 +686,7 @@ include "connect.php";
 				<span><strong>Sortieren nach</strong></span>
 				
 				<select class="form-control" name="orderBy">
-					<option value="overall" <?php if(isset($sortSelection['overall'])) echo $sortSelection['overall']?>>Gesamtbewertung</option>
+					<option value="overall" <?php if(isset($sortSelection['overall'])) echo $sortSelection['overall']?>>Gesamtwertung</option>
 					<option value="crit1" <?php if(isset($sortSelection['crit1'])) echo $sortSelection['crit1']?>>Bewertung Veranstaltung</option>
 					<option value="crit2" <?php if(isset($sortSelection['crit2'])) echo $sortSelection['crit2']?>>Bewertung Aufwand</option>
 					<option value="crit3" <?php if(isset($sortSelection['crit3'])) echo $sortSelection['crit3']?>>Bewertung Kriterium 3</option>
