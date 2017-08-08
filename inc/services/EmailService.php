59571542baed662807050c1a00b6e46fe75d43eb
jonas-l-b <ueehp@student.kit.edu>
2017-08-08 09:05:44 +0200

    works on EmailService

--- inc/services/EmailService.php
+++ inc/services/EmailService.php
@@ -45,6 +45,20 @@
 	   protected function __construct() {
 		   new PHPMailerAutoload();
 		   $mail = new PHPMailer();
+		   
+		   $mail->SMTPDebug = 3;			//Gibt starke Debugging Ausgaben aus - für Realease Deaktivieren (später auf 2 vgl. Homepage2016)
+		   $mail->setLanguage('de');
+		   $mail->IsSendmail();
+           $mail->Host = "REPLACE WITH HOST";
+           $mail->Port = "PEPLACE WITH PORT";
+           $mail->SMTPSecure = "ssl";
+           $mail->SMTPAuth = true;
+           $mail->Username = "REPLACE WITH USERNAME";
+           $mail->Password = "REPLACE WiTH PASSWORD";
+           $mail->From       = "REPLACE WITH EMAIL-ADDRESS";
+           $mail->FromName   = "Studienführer - VWI-ESTIEM-Karlsruhe e.V.";
+           $mail->CharSet =  'UTF-8';  
+		   $mail->isHTML(true);
 	   }
 	   
 	   
@@ -53,13 +67,33 @@
 		*
 		* Sendet eine Email an den Nutzer. Gibt ein gewisses Format vor
 		*/
-	   public function sendEmail(){
-		   
-		   
-		   if(mail($to, $subject, $message, $headers)){
-			   return true;
-		   }else{
+	   public function sendEmail($toEmail, $userName, $subject, $body){
+		   $mail->AddAddress($toEmail, $userName);
+		   $mail->Subject = $subject;
+		   $mail->Body = "
+			<html>
+				<header>
+					<style>
+					
+					</style>
+				</header>
+				<body>
+					<div style=\"font-family:calibri\">
+						<h1>$subject</h1>
+						<p>Hallo $userName,</p>
+						$body
+						<p>Viel Spaß mit dem Studienführer,<br>
+						Deine VWI-ESTIEM Hochschulgruppe Karlsruhe</p>
+						<br><br>
+						<p></p>
+					</div>
+				</body>
+			</html>
+			";
+		   if(!$mail->Send()){
 			   return false;
+		   }else{
+			   return true;
 		   }
 	   }
 	}
