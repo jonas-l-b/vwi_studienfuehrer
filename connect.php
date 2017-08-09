87360b508546b6101edc8e200ccd4e6e594f27c4
juliangermek <30285755+juliangermek@users.noreply.github.com>
2017-08-09 15:14:50 +1000

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,19 +1,19 @@
 <?php
 
-
+/*
 $user = 'root';
 $pass = '';
 $db = 'studienfuehrer';
 $con = mysqli_connect('localhost', $user, $pass, $db) or die("Unable to connect to database!");
 $con->set_charset("utf8");
+*/
 
-/*
 $user = 'YOUR USER';
 $pass = 'YOUR PASSWORD';
 $db = 'YOUR DATABSE';
 $con = mysqli_connect('YOUR LINK', $user, $pass, $db) or die("Unable to connect to database!");
 $con->set_charset("utf8");
-*/
+
 
 //if($con) echo ("<br> Sucessfully connected to database $db."); //CHECK
 
