f36aefc1e53d6829f8e6e21669045ab1187f9b6e
juliangermek <julian.germek@gmail.com>
2017-08-16 09:59:16 +1000

    change star position

--- index.php
+++ index.php
@@ -196,7 +196,8 @@ include "sumVotes.php";
 
 	<!--Überschrift und Info-->
 	<div style="margin-bottom:20px; padding:20px 20px 0px 0px;">
-		<h1> <?php echo $subjectData['subject_name'] ?> </h1>
+		<h1> <?php echo $subjectData['subject_name'] ?> <span id="favIcon" class="glyphicon glyphicon-star-empty favouriteStar"></span> </h1>
+		
 	</div>
 	
 	<div class="infoContainer">
@@ -236,16 +237,16 @@ include "sumVotes.php";
 	?>
 	<div style="margin-bottom:15px">
 		<button <?php echo $displayRatings ?> type="button" a href="#myModal" role="button" class="btn btn-primary" data-toggle="modal" <?php if(isset($ratingButtonDisabled)) echo $ratingButtonDisabled?>><?php echo $ratingButtonText ?></button>
-		<span id="favIcon" class="btn glyphicon glyphicon-star-empty favouriteStar"></span>
 	</div>
 	
 	<script>
 	$(document).ready(function(){
 		$("#favIcon").click(function(){
 			$("#favIcon").attr("style", "color:rgb(255, 204, 0)");
-			$("#favIcon").attr("class", "btn glyphicon glyphicon-star favouriteStar");
+			$("#favIcon").attr("class", "glyphicon glyphicon-star favouriteStar");
 		});
 	});
+	$.post( "favourties_newEntry.php" );
 	</script>
 
 
