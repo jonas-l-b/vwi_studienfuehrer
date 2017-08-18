9c80df2fa0070889a0f341dd7dbb6116bf7fe5b7
juliangermek <julian.germek@gmail.com>
2017-08-18 11:10:00 +1000

    adds links to tabs

--- userProfile.php
+++ userProfile.php
@@ -220,14 +220,24 @@ include "connect.php";
 		</div>
 	-->
 	</div>
-	
-	
-	
-	
-	
-
-
 </div>
+
+<script>
+// Javascript to enable link to tab
+var url = document.location.toString();
+if (url.match('#')) {
+	$('.nav-tabs a[href="#'+url.split('#')[1]+'"]').tab('show') ;
+} 
+
+// With HTML5 history API, we can easily prevent scrolling!
+$('.nav-tabs a').on('shown.bs.tab', function (e) {
+	if(history.pushState) {
+		history.pushState(null, null, e.target.hash); 
+	} else {
+		window.location.hash = e.target.hash; //Polyfill for old browsers
+	}
+})
+</script>
 
 <!-- End of page. Modal für Passwort vergessen -->
 <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
