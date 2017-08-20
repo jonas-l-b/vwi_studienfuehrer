00d032166e3bc635c58dc6017c42f577a1a34a43
jonas-l-b <ueehp@student.kit.edu>
2017-08-20 21:36:32 +0200

    entfernt unnötige logs

--- connect.php
+++ connect.php
@@ -13,7 +13,6 @@
 	
 	$logger = new Logger('studienfuehrer_logger');
 	$logger->pushHandler(new StreamHandler(__DIR__.'/logs/studienfuehrer.log', Logger::DEBUG));
-	$logger->info('Neue Studienfuehrer Session');
 //TEST ENDE
 
 $configs = ConfigService::getService()->getConfigs();
@@ -24,6 +23,5 @@ if(!$con = mysqli_connect($configs['db_url'], $user, $pass, $db)){
   $logger->info('Kann keine Verbindung mit der Datenbank herstellen.');
   die("Kann keine Verbindung mit der Datenbank herstellen.");
 }
-$logger->info('Verbindung mit Datenbank hergestellt.');
 $con->set_charset("utf8");
 ?>
