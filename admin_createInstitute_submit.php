175435ba945a1ae28317a8293e9fb1b31dadc3a7
jonas-l-b <ueehp@student.kit.edu>
2017-08-20 21:34:27 +0200

    fügt erste logs hinzu

--- admin_createInstitute_submit.php
+++ admin_createInstitute_submit.php
@@ -28,6 +28,7 @@ if (mysqli_num_rows($result1)==0){
 	";
 
 	mysqli_query($con,$sql2);
+	$logger->info("Neues Institut hinzugefügt: $inst_name $inst_abbr von User: $userID" );
 
 	$insti_new_selection = "";
 
--- connect.php
+++ connect.php
@@ -8,12 +8,22 @@
 		//'cache' => 'templates/cache',
 		'cache' => false,
 	));
+	use Monolog\Logger;
+	use Monolog\Handler\StreamHandler;
+	
+	$logger = new Logger('studienfuehrer_logger');
+	$logger->pushHandler(new StreamHandler(__DIR__.'/logs/studienfuehrer.log', Logger::DEBUG));
+	$logger->info('Neue Studienfuehrer Session');
 //TEST ENDE
 
 $configs = ConfigService::getService()->getConfigs();
 $user = $configs['db_user'];
 $pass = $configs['db_password'];
 $db = $configs['db_database'];
-$con = mysqli_connect($configs['db_url'], $user, $pass, $db) or die("Kann keine Verbindung mit der Datenbank herstellen.");
+if(!$con = mysqli_connect($configs['db_url'], $user, $pass, $db)){
+  $logger->info('Kann keine Verbindung mit der Datenbank herstellen.');
+  die("Kann keine Verbindung mit der Datenbank herstellen.");
+}
+$logger->info('Verbindung mit Datenbank hergestellt.');
 $con->set_charset("utf8");
 ?>
