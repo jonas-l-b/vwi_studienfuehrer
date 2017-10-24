802eabe3543f6cbc46c09d25f374e2db55091a2b
jonas-l-b <ueehp@student.kit.edu>
2017-10-24 02:01:34 +0200

    beautify lange nacht des wissens

--- langeNachtDesWissensStats.php
+++ langeNachtDesWissensStats.php
@@ -5,7 +5,8 @@ include('connect.php');
 include('header.php');
 
 ?>
-<body>
+<body style="background-image: url('res/img/night.jpg'); background-repeat: no-repeat;background-position: right top;
+    background-attachment: fixed;color:#fff !important; background-size: cover;">
 <div class="container">
 	<br />
 	<br />
@@ -18,34 +19,34 @@ include('header.php');
 <div class="col-md-6">
 	<div class="ui statistics" style="text-align:center">
   <div class="statistic">
-    <div class="value" id="dozVal">
+    <div class="value" id="dozVal" style="color:#fff !important;">
       0
     </div>
-    <div class="label">
+    <div class="label" style="color:#fff !important;">
       Dozenten
     </div>
   </div>
   <div class="statistic">
-    <div class="value" id="verVal">
+    <div class="value" id="verVal" style="color:#fff !important;">
       0
     </div>
-    <div class="label">
+    <div class="label" style="color:#fff !important;">
       Veranstaltungen
     </div>
   </div>
   <div class="statistic">
-    <div class="value" id="modVal">
+    <div class="value" id="modVal" style="color:#fff !important;">
       0
     </div>
-    <div class="label">
+    <div class="label"style="color:#fff !important;">
       Module
     </div>
   </div>
   <div class="statistic">
-    <div class="value" id="insVal">
+    <div class="value" id="insVal" style="color:#fff !important;">
       0
     </div>
-    <div class="label">
+    <div class="label" style="color:#fff !important;">
       Institute
     </div>
   </div>
@@ -62,25 +63,25 @@ include('header.php');
 			$.get( "langeNachtDesWissensStatsAPI.php",
 					 function( data ) {
 			  data = JSON.parse(data);
-				var numAnim = new CountUp("dozVal", $( "#dozVal" ).text() , data.dozVal, 0,data.dozVal-$( "#dozVal" ).text());
+				var numAnim = new CountUp("dozVal", $( "#dozVal" ).text() , data.dozVal, 0,(data.dozVal-$( "#dozVal" ).text())*2);
 				if (!numAnim.error) {
 					numAnim.start();
 				} else {
 					console.error(numAnim.error);
 				}
-				var numAnim = new CountUp("modVal", $( "#modVal" ).text() , data.modVal, 0,data.modVal-$( "#modVal" ).text());
+				var numAnim = new CountUp("modVal", $( "#modVal" ).text() , data.modVal, 0,(data.modVal-$( "#modVal" ).text())*2);
 				if (!numAnim.error) {
 					numAnim.start();
 				} else {
 					console.error(numAnim.error);
 				}
-				var numAnim = new CountUp("verVal", $( "#verVal" ).text() , data.verVal, 0, data.verVal-$( "#verVal" ).text());
+				var numAnim = new CountUp("verVal", $( "#verVal" ).text() , data.verVal, 0, (data.verVal-$( "#verVal" ).text())*2);
 				if (!numAnim.error) {
 					numAnim.start();
 				} else {
 					console.error(numAnim.error);
 				}
-				var numAnim = new CountUp("insVal", $( "#insVal" ).text() , data.insVal, 0, data.insVal-$( "#insVal" ).text());
+				var numAnim = new CountUp("insVal", $( "#insVal" ).text() , data.insVal, 0, (data.insVal-$( "#insVal" ).text())*2);
 				if (!numAnim.error) {
 					numAnim.start();
 				} else {
