16c250195ce151537158c7a45ecae7b7bb8bbdee
jonas-l-b <ueehp@student.kit.edu>
2017-11-09 17:31:30 +0100

    quickfix #117
    (bisschen improvisiert, aber eigentlich zielführend mMn)

--- loadSubjectData.php
+++ loadSubjectData.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $result = mysqli_query($con,"SELECT * FROM subjects WHERE ID = '$subject'");
 $num = mysqli_num_rows($result);
@@ -7,7 +7,7 @@ $num = mysqli_num_rows($result);
 if ($num >= 1 ) {
 	$subjectData = mysqli_fetch_assoc($result);
 } else {
-	echo ("<SCRIPT LANGUAGE='JavaScript'>window.location.href='landing.php?m=no_subject_in_db';</SCRIPT>");
+	echo ("<SCRIPT LANGUAGE='JavaScript'>window.location.href='tree.php';</SCRIPT>");
 }
 
 ?>
