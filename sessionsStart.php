eb60c23330695ce721b1aea04677fa0e100330a1
jonas-l-b <ueehp@student.kit.edu>
2018-02-02 15:18:17 +0100

    bugfix logout

--- sessionsStart.php
+++ sessionsStart.php
@@ -13,7 +13,7 @@ function endsWith1($haystack, $needle)
 
 if (!isset($_SESSION['userSession'])) {
   $url =  urlencode((isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
-	if(endsWith1($url, urlencode('vwi-karlsruhe.de')) || endsWith1($url, urlencode('vwi-karlsruhe.de/')) || endsWith1($url, urlencode('login.php')) || endsWith1($url, urlencode('vwi_studienfuehrer/'))){
+	if(endsWith1($url, urlencode('vwi-karlsruhe.de')) || endsWith1($url, urlencode('vwi-karlsruhe.de/')) || endsWith1($url, urlencode('login.php')) || endsWith1($url, urlencode('vwi_studienfuehrer/')) || endsWith1($url, urlencode('logout.php'))){
 		echo ("<SCRIPT LANGUAGE='JavaScript'>window.location.href='login.php';</SCRIPT>");
 	}else{
 		echo ("<SCRIPT LANGUAGE='JavaScript'>window.location.href='login.php?url=$url';</SCRIPT>");
