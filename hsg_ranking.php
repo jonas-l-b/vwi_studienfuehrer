3f8996a28905aa02441cf88f5bca69c9d5a5a141
juliangermek <julian.germek@gmail.com>
2018-06-18 20:44:18 +0200

    hsg ranking update

--- hsg_ranking.php
+++ hsg_ranking.php
@@ -13,33 +13,59 @@ include "connect.php";
 
 <div class="container">
 	<h2>Hochschulgruppen-Ranking</h2>
-	<h4>Wer hat am fleißigsten bewertet?</h4>
-	<br>
-	<?php
-	$sql="
-		SELECT ratings.user_ID AS user_ID, username, COUNT(ratings.user_ID) AS count FROM ratings
-		JOIN users ON ratings.user_ID = users.user_ID
-		GROUP BY ratings.user_ID
-		ORDER BY COUNT(ratings.user_ID) DESC
-	";
-	$result = mysqli_query($con, $sql);
 
-	
-	$i=1;
-	while($row = mysqli_fetch_assoc($result)){
-		if($row['count']>1){
-			$r="Bewertungen";
-		}else{
-			$r="Bewertung";
-		}
-		echo "<h4>Platz ".$i.": ".$row['username']." (".$row['count']." ".$r.")</h4>";
-		if($i==3){
-			echo "<hr style=\"border-color:grey; margin-bottom:0\">";
-			echo "<p>(Wer am 05. Juni am Ende der Sitzung über dieser Linie steht, bekommt ein Bier im Ox!)";
-		}
-		$i++;
-	}
-	?>
+	<div class="row">
+		<div class="col-md-6">		
+			<h3>Bewertungen dieser Woche (Mi bis Di)</h3>
+
+			<?php
+			$sql="
+				SELECT ratings.user_ID AS user_ID, username, COUNT(ratings.user_ID) AS count FROM ratings
+				JOIN users ON ratings.user_ID = users.user_ID
+				WHERE time_stamp >= DATE_ADD(CURDATE(), INTERVAL -(WEEKDAY(CURDATE())-3) DAY)
+				GROUP BY ratings.user_ID
+				ORDER BY COUNT(ratings.user_ID) DESC
+			";
+			$result = mysqli_query($con, $sql);
+
+			
+			$i=1;
+			while($row = mysqli_fetch_assoc($result)){
+				if($row['count']>1){
+					$r="Bewertungen";
+				}else{
+					$r="Bewertung";
+				}
+				echo "<h4>Platz ".$i.": ".$row['username']." (".$row['count']." ".$r.")</h4>";
+				$i++;
+			}
+			?>
+		</div>
+		<div class="col-md-6">	
+			<h3>Gesamt-Ranking</h3>
+			<?php
+			$sql="
+				SELECT ratings.user_ID AS user_ID, username, COUNT(ratings.user_ID) AS count FROM ratings
+				JOIN users ON ratings.user_ID = users.user_ID
+				GROUP BY ratings.user_ID
+				ORDER BY COUNT(ratings.user_ID) DESC
+			";
+			$result = mysqli_query($con, $sql);
+
+			
+			$i=1;
+			while($row = mysqli_fetch_assoc($result)){
+				if($row['count']>1){
+					$r="Bewertungen";
+				}else{
+					$r="Bewertung";
+				}
+				echo "<h4>Platz ".$i.": ".$row['username']." (".$row['count']." ".$r.")</h4>";
+				$i++;
+			}
+			?>
+		</div>
+	</div>
 	<br>
 </div>
 </body>
