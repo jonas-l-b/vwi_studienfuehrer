42c0d58d5896f155605bff4ae90cccaadf989aba
juliangermek <julian.germek@gmail.com>
2018-10-20 18:26:07 +0200

    icons

--- myachievements.php
+++ myachievements.php
@@ -18,7 +18,17 @@ include "connect.php";
 <div class="container">
 	<h1 style="text-align:center"><i class="fa fa-trophy" style="color:#FACC2E"></i> <u>Meine Errungenschaften</u> <i class="fa fa-trophy" style="color:#FACC2E"></i></h1>
 
-	<h3 style="text-align:center">Gesamtpunktzahl: 1000 Punkte</h3>
+	<?php
+	$sql="
+		SELECT sum(badges.points) AS sum_of_points FROM badges
+		JOIN users_badges ON badges.id = users_badges.badge_id
+		WHERE users_badges.user_id = ".$userRow['user_ID']."
+	";
+	$result=mysqli_query($con, $sql);
+	$row = mysqli_fetch_assoc($result);
+	?>
+	<h3 style="text-align:center">Gesamtpunktzahl: <?php echo $row['sum_of_points']?> Punkte</h3>
+	<p style="text-align:center">Vergleiche dich mit anderen: <a href="achievements.php">Zum Ranking</a></p>
 	<br>
 
 	<div style="text-align:center;">
@@ -27,28 +37,39 @@ include "connect.php";
 		$sql="
 			SELECT * FROM badges
 			LEFT JOIN users_badges ON badges.id = users_badges.badge_id
-			WHERE users_badges.user_id = 2 OR users_badges.user_id IS NULL"
-		;
+			ORDER BY badges.id
+		";
 		$result=mysqli_query($con, $sql);
 		
 		while($row = mysqli_fetch_assoc($result)){
-			if(!is_null($row['user_id'])){
+			if($row['user_id'] == $userRow['user_ID']){
 				$color = "rgb(20,90,157)";
 				$blurry = "";
 				$name = $row['name'];
 				$description = $row['description'];
+				$blurryimage = "";
 			}else{
 				$color = "lightgrey";
 				$blurry = "class=\"blurry\"";
 				$name = "This name, you know!";
 				$description = "Don't use source code to spy on badges!";
+				$blurryimage = "blurryimage";
+				
+				//For development, please keep uncomment
+				/*
+				$color = "rgb(20,90,157)";
+				$blurry = "";
+				$name = $row['name'];
+				$description = $row['description'];
+				$blurryimage = "";
+				*/
 			}
 			
 			?>
 			<div style="border: solid lightgrey 3px; width: 330px; padding: 5px; background-color:#f2f2f2; display: inline-block; margin:5px;">
 				<table style="width:100%">
 					<tr>
-						<td style="width:1%; padding:5px;"><img src="pictures/badges/<?php echo $row['image']?>" class="media-object custom-media" style="width:80px; background:<?php echo $color?>; border: 4px solid white; padding:5px;"></td>
+						<td style="width:1%; padding:5px;"><img src="pictures/badges/<?php echo $row['image']?>" class="media-object <?php echo $blurryimage?>" style="width:80px; background:<?php echo $color?>; border: 4px solid white; padding:5px;"></td>
 						<td>
 							<table style="width:100%">
 								<tr>
