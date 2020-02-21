f2b05af7e65a6cc665077c08cf30b2f65c152c73
julian <30285755+juliangermek@users.noreply.github.com>
2020-02-21 12:58:32 +0100

    Create posting.php

---
+++ posting.php
@@ -0,0 +1,75 @@
+<?php
+include "sessionsStart.php";
+include "header.php";
+?>
+
+<body>
+
+<?php include "inc/nav.php" ?>
+
+<div class="container">
+
+	<h2>Bist du der neue Kopf hinter dem Studienführer?</h2>
+	
+	<p>
+		Die Köpfe hinter dem Studienführer stehen vor dem Ende ihres Studentenlebens.
+		Darum sind wir auf der Suche nach einer <b>passenden Nachfolgerin</b>*, die sich um den Studienführer kümmert und Lust hat, ihn weiterzuentwickeln.
+	</p>
+	
+	<p>
+		Der Studienführer ist ein <b>Projekt einer Hochschulgruppe des KIT</b>, der <b><a href="https://www.vwi-karlsruhe.de/">Hochschulgruppe VWI-ESTIEM Karlsruhe</a></b>.
+		Du würdest also in diese Hochschulgruppe eintreten wärst damit nicht allein, sondern hättest eine Gruppe Kommilitonen und gegebenenfalls auch Budget, die und das dich unterstützt.
+		Wir suchen also jemanden, die sich sozusagen <b>den Hut aufsetzt, aber keinesfalls alles alleine macht</b>.
+	</p>
+	
+	<br>
+	
+	<h4>Was du dafür mitbringen solltest</h4>
+	<ul style="margin-left:25px">
+		<li><b>Interesse am Projekt "Studienführer" an sich</b></li>
+		<li>Von Vorteil: Interesse an Webprogrammierung und Datenbanken</li>
+	</ul>
+	
+	<h4 style="margin-top:15px">Was du davon hast</h4>
+	<ul style="margin-left:25px">
+		<li>Die Möglichkeit, <b>spielend leicht mit Learning-by-Doing Hard-Skills zu sammeln</b> (Programmierung, Datenbanken), die in Bewerbungsgespräch und Job helfen können</li>
+		<li>Die Möglichkeit, <b>Team- und Projektarbeit in einer sicheren Umgebung auszuprobieren</b> (besser auf unserer Spielwiese lernen, als im ersten Job)</li>
+		<li>Die Möglichkeit, andere Wiwis mit den von dir erstellten neuen Funktionen zu beeindrucken - Der Studienführer umfasst mehr als 1000 Nutzer!</li>
+	</ul>
+	
+	<br>
+	
+	<div style="border-left: solid 5px grey; border-radius:3px;margin: 5px;">
+		<div style="padding:10px;">
+			<b>TL;DR</b> (Zusammenfassung): <b>Wir suchen jemanden, die das Projekt cool findet und sich engagieren will.</b>
+		</div>
+	</div>
+	
+	<br>
+	
+	<p>
+		<b>Wie würde es weitergehen?</b>
+		Wir würden uns einfach mal zusammensetzen und quatschen.
+		Falls das dann immmernoch passt, würdest du die Hochschulgruppe und den Vorstand kennenlernen.
+		Außerdem würden wir dir zeigen, wo alles zu finden ist und wie es funktioniert.
+		Dann würden wir dir den Hut übergeben, sodass du loslegen kannst.
+		Um mit den anderen in Kontakt zu bleiben, würdest du häufiger an den Sitzungen der Hochschulgruppe (immer dienstags) teilnehmen.
+	</p>
+	
+	<br>
+
+	<div class="well">
+		<b>Interesse? Das wäre was für dich? Dann schreib uns: <a href="mailto:julian.germek@estiem.org">julian.germek@estiem.org</a></b>.
+	</div>
+	
+	<br>
+	
+	<p>
+		*Wir nutzen die weibliche Form und schließen alle Geschlechter ein.
+		Das tun wir, da wir wissen, dass Frauen sich tendenziell weniger häufig bewerben, wenn sie nicht alle Anforderungen erfüllen (<a href="https://hbr.org/2014/08/why-women-dont-apply-for-jobs-unless-theyre-100-qualified" target="_blank">Quelle</a>).
+		Wir können uns nämlich sehr gut auch eine Frau als Kopf hinter dem Studienführer vorstellen!
+	</p>
+	
+</div>
+
+</body>
